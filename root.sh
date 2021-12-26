a591b9923e720b188f0981384d3386e9ebdf99d5
dp vpn <96043348+Wenisaputri@users.noreply.github.com>
2021-12-26 08:07:04 +0700

    Update root.sh

--- root.sh
+++ root.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -O /etc/ssh/sshd_config "https://raw.githubusercontent.com/Gugun09/root/main/sshd.conf"
+wget -O /etc/ssh/sshd_config "https://raw.githubusercontent.com/Wenisaputri/root/main/sshd.conf"
 /etc/init.d/ssh restart
 passwd root
